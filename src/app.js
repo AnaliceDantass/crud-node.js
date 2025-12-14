1d5720fb6fe1fb93a54798ea2a870c189f49a763
Analice Dantas <analicedantas07@gmail.com>
2025-12-14 19:11:22 -0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,3 +1,4 @@
+import { error } from 'console'
 import express from 'express'
 import path from 'path'
 import { fileURLToPath } from 'url'
@@ -5,23 +6,24 @@ import { fileURLToPath } from 'url'
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 
+const raiz = path.resolve(__dirname,'..')
+
 const app = express() 
 
-app.use(express.static('public'))
+app.use(express.static(path.join(raiz,'public')))
 app.use(express.json())
-app.use(express.urlencoded({extended:true}))
 
 app.get('/',(req,res)=>{
-    res.sendFile(path.join(__dirname,'index.html'))
+    res.sendFile(path.join(raiz, 'public','index.html'))
 })
 
 app.post('/api/login',(req,res)=>{
-    const {username,password} = req.body        
+    const {usuario,senha} = req.body        
 
-    if(username === 'Admin' && password === '@adminVittaPrime!'){
-        res.json({message: 'Usuário autenticado!'})
+    if(usuario === 'Admin' && senha === '@adminVittaPrime!'){
+        res.json({error: 'Usuário autenticado!'})
     } else {
-        res.status(401).json({message: 'Erro de autenticação'})
+        res.status(401).json({error: 'Erro de autenticação'})
     }      
 })
 
